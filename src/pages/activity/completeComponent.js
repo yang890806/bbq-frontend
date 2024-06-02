812aa5b58936a8e1fcd631555487641d3298e3bd
momotsaiii <momotsaiii@gmail.com>
2024-06-03 03:23:51 +0800

    加入圖片顯示連結

--- src/pages/activity/completeComponent.js
+++ src/pages/activity/completeComponent.js
@@ -4,7 +4,11 @@ function CompleteComponent() {
   return (
     <div className="flex justify-center items-start">
       <div className="flex flex-row space-x-6 p-2">
-        <div className="w-96 h-80 bg-gray-300 shadow-md rounded-[10px]"></div>
+        <img
+          src="https://i.pinimg.com/564x/b0/c1/69/b0c169df50ecc6c6985cb5d10c52f26d.jpg"
+          alt="替代文字"
+          className="w-96 h-80 bg-gray-300 shadow-md rounded-[10px]"
+        />
         <div className="bg-white p-6 shadow-md">
           <p className="text-gray-700 text-base mb-4 w-100">
             Story text Story text Story text Story text Story text Story text
--- src/pages/activity/introduction.js
+++ src/pages/activity/introduction.js
@@ -3,7 +3,11 @@ import React from "react";
 function Introduction() {
   return (
     <div className="flex items-center space-x-6 mb-10">
-      <div className="bg-gray-300 shadow-sm h-40 w-32 rounded-[10px] min-h-[10rem] min-w-[8rem] max-h-[10rem] max-w-[8rem]"></div>{" "}
+      <img
+        src="https://i.pinimg.com/736x/d9/9d/92/d99d920205b664c8ccb672b2c6fdf107.jpg"
+        alt="替代文字"
+        className="bg-gray-300 shadow-sm h-40 w-32 rounded-[10px] min-h-[10rem] min-w-[8rem] max-h-[10rem] max-w-[8rem]"
+      />
       <div>
         <h1 className="text-lg font-semibold">Book Name</h1>
         <p className="text-gray-500">author (目前所有的作者)</p>
